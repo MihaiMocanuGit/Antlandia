37977522bfd6b10cc7ced17b4e04401fd925bef5
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2022-09-28 23:50:07 +0300

    implemented sfml into chunk header

--- src/chunk.h
+++ src/chunk.h
@@ -1,8 +1,8 @@
 #ifndef CHUNK_H
 #define CHUNK_H
 
-
 #include <array>
+#include <SFML/Window.hpp>
 #include "ant.h"
 
 
@@ -11,27 +11,28 @@ class Chunk
 	private:
 		static constexpr unsigned int m_MAX_ANTS_CHUNK = 255;
 
-		void initNearbyChunks(int noOfChunksY, int noOfChunksX);
+		void initNearbyChunkMap(sf::Vector2i noOfChunks);
 	public:
 		std::array<Ant, m_MAX_ANTS_CHUNK> antsInChunk;
-	    int chunkIndexX, chunkIndexY;
-		
-        static constexpr int CHUNK_SIZE_X = 128;
-		static constexpr int CHUNK_SIZE_Y = 128;
+	    
+		sf::Vector2i chunkIndex;
+
+		//needs to be constexpr/const/private with get
+        sf::Vector2i chunkSize = {128, 128};
 
 		Chunk *pNearbyChunks[3][3];
         
         Chunk();       
-		Chunk(int aChunkIndexX, int aChunkIndexY);
+		Chunk(sf::Vector2i aChunkIndex);
 		
         
-		///calls initNearbyChunks for every chunk in 2D array. It inits *pNearbyChunks[3][3];
+		///calls initNearbyChunkMap for every chunk in 2D array. It inits *pNearbyChunks[3][3];
         template<std::size_t SIZE_X, std::size_t SIZE_Y>
-		static void chunksInit(std::array<std::array<Chunk, SIZE_X>, SIZE_Y> &chunks)
+		static void initAllChunks(std::array<std::array<Chunk, SIZE_X>, SIZE_Y> &chunks)
 		{
 			for(auto &chunksY : chunks)
         		for(auto &chunkYX : chunksY)
-            		chunkYX.initNearbyChunks(chunksY.size(), chunks.size());
+            		chunkYX.initNearbyChunkMap(chunksY.size(), chunks.size());
 		}
 
 			
