21de1c01ecf8c760e42d5756768f7ad3ddbbf1e9
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2022-11-19 22:41:54 +0200

    updated TODO with more tasks

---
+++ src/pheromone.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mihai on 11/19/22.
+//
+
+#include "Pheromone.h"
---
+++ src/pheromone.h
@@ -0,0 +1,14 @@
+//
+// Created by mihai on 11/19/22.
+//
+
+#ifndef ANTSIMULATION_PHEROMONE_H
+#define ANTSIMULATION_PHEROMONE_H
+
+
+class Pheromone {
+
+};
+
+
+#endif //ANTSIMULATION_PHEROMONE_H
