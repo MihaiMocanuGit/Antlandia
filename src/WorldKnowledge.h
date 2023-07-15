12c4fadb68177214c6e27dcfeb5edd4eefb14b00
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2023-07-15 21:46:53 +0300

    converted worldKnowledge to a template

--- src/WorldKnowledge.h
+++ src/WorldKnowledge.h
@@ -3,21 +3,24 @@
 #include <vector>
 
 #include "Types.h"
+template <class T>
 class Chunk;
+
 class World;
 
+template <class T>
 class WorldKnowledge
 {
-    template <typename T>
+    template <typename U>
     friend class SpecializedVector;
 private:
     World *m_pWorld = nullptr;
-    Chunk *m_pHomeChunk = nullptr;
+    Chunk<T> *m_pHomeChunk = nullptr;
 
     size_t m_indexChunk = -1; //size_t max
     size_t m_indexWorld = -1;
 public:
     WorldKnowledge() = default;
-    explicit WorldKnowledge(World *pWorld, Chunk *pHomeChunk) : m_pWorld(pWorld), m_pHomeChunk(pHomeChunk)
+    explicit WorldKnowledge(World *pWorld, Chunk<T> *pHomeChunk) : m_pWorld(pWorld), m_pHomeChunk(pHomeChunk)
     {}
 };
