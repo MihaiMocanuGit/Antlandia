932870f4309a3e6e9519fcb20d59325c54c5efbe
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2023-07-25 21:37:01 +0300

    separated initialization of ants into another function, made it visible only in it's file scope

--- src/Antlandia.h
+++ src/Antlandia.h
@@ -2,25 +2,33 @@
 #include "ChunkMap.h"
 #include "AppLogic.h"
 
-void startApp()
+namespace
 {
-    World world(10, 10);
-
+void m_addObjects(World &world)
+{
     for (int i = 0; i < 125; ++i)
     {
         world.addAnt(sf::Vector2f(i * 5.0f, i * 5.0f));
         world.addFood(sf::Vector2f(world.size().x * Chunk<void>::CHUNK_SIZE_X - i * 5.0f, i * 5.0f));
 
         if (i % 2 == 0)
-            world.addPheromone(sf::Vector2f((float)world.size().x * Chunk<void>::CHUNK_SIZE_X / 2, i * 5.0f));
+            world.addPheromone(sf::Vector2f((float) world.size().x * Chunk<void>::CHUNK_SIZE_X / 2, i * 5.0f));
         else
-            world.addPheromone(sf::Vector2f(i * 5.0f, (float)world.size().y * Chunk<void>::CHUNK_SIZE_Y / 2));
+            world.addPheromone(sf::Vector2f(i * 5.0f, (float) world.size().y * Chunk<void>::CHUNK_SIZE_Y / 2));
     }
 
 
     world.ants().addAll();
     world.food().addAll();
     world.pheromones().addAll();
+}
+
+} //end anonymous namespace
+void startApp()
+{
+    World world(10, 10);
+    m_addObjects(world);
+
     startGameLoop(world);
 }
 //#include "objects/GenericObject.h"
