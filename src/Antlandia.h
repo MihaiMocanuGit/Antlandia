ff60dfa09f6fac3dcfc48514f3c77598c0ec393f
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2023-07-25 21:59:34 +0300

    added source file to header

--- src/Antlandia.h
+++ src/Antlandia.h
@@ -2,33 +2,6 @@
 #include "ChunkMap.h"
 #include "AppLogic.h"
 
-namespace
-{
-void m_addObjects(World &world)
-{
-    for (int i = 0; i < 125; ++i)
-    {
-        world.addAnt(sf::Vector2f(i * 5.0f, i * 5.0f));
-        world.addFood(sf::Vector2f(world.size().x * Chunk<void>::CHUNK_SIZE_X - i * 5.0f, i * 5.0f));
 
-        if (i % 2 == 0)
-            world.addPheromone(sf::Vector2f((float) world.size().x * Chunk<void>::CHUNK_SIZE_X / 2, i * 5.0f));
-        else
-            world.addPheromone(sf::Vector2f(i * 5.0f, (float) world.size().y * Chunk<void>::CHUNK_SIZE_Y / 2));
-    }
+void startApp();
 
-
-    world.ants().addAll();
-    world.food().addAll();
-    world.pheromones().addAll();
-}
-
-} //end anonymous namespace
-void startApp()
-{
-    World world(10, 10);
-    m_addObjects(world);
-
-    startGameLoop(world);
-}
-//#include "objects/GenericObject.h"
