1e3bb5051af7ae34ae0ac6ac05e439ecfafaf005
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2023-08-16 16:58:05 +0300

    modified color values

--- src/ObjectTypes/PheromoneTypes.h
+++ src/ObjectTypes/PheromoneTypes.h
@@ -10,7 +10,7 @@ public:
     PheromoneTypes(World *pWorld, SpecializedVector<Pheromone> *pWorldVector, PrimitiveChunkMap_t<Pheromone> *pPrimitiveChunkMap);
     const WorldKnowledge<Pheromone> KNOWLEDGE;
 
-    const Body TRAIL_PHEROMONE_BODY = {sf::Vector2f{-1.0f, -1.0f}, 1.0f, 1.0f,
+    const Body TRAIL_PHEROMONE_BODY = {sf::Vector2f{-1.0f, -1.0f}, 1.5f, 1.0f,
                                        sf::Color{0, 255, 0, 255}};
     const Pheromone TRAIL_PHEROMONE = {TRAIL_PHEROMONE_BODY, KNOWLEDGE, Pheromone::Type_e::Trail};
 
@@ -19,8 +19,8 @@ public:
     const Pheromone FOOD_PHEROMONE = {FOOD_PHEROMONE_BODY, KNOWLEDGE, Pheromone::Type_e::Food};
 
 
-    const Body HOME_PHEROMONE_BODY = {sf::Vector2f{-1.0f, -1.0f}, 1.0f, 15.0f,
-                                      sf::Color{0, 255, 255, 255}};
+    const Body HOME_PHEROMONE_BODY = {sf::Vector2f{-1.0f, -1.0f}, 1.5f, 15.0f,
+                                      sf::Color{0, 127, 255, 255}};
     const Pheromone HOME_PHEROMONE = {HOME_PHEROMONE_BODY, KNOWLEDGE, Pheromone::Type_e::Home,
                                       Pheromone::DEFAULT_HALF_LIFE * 4};
 };
