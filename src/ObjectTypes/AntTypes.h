a03385be448df6d9a0f3f38a4c825ff10ce2a104
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2023-09-01 00:06:04 +0300

    default worker ants now start in searching mode

--- src/ObjectTypes/AntTypes.h
+++ src/ObjectTypes/AntTypes.h
@@ -12,15 +12,15 @@ public:
 
     const Body WORKER_ANT_BODY = {sf::Vector2f{-1.0f, -1.0f}, 2.0f, 1.0f,
                                        sf::Color{0, 0, 0, 255}};
-    const Ant WORKER_ANT = {WORKER_ANT_BODY, KNOWLEDGE, Ant::Type_e::Worker};
+    const Ant WORKER_ANT = {WORKER_ANT_BODY, KNOWLEDGE, Ant::Action_e::SearchingFood};
 
     const Body MALE_ANT_BODY = {sf::Vector2f{-1.0f, -1.0f}, 1.5f, 1.0f,
                                   sf::Color{50, 0, 150, 255}};
-    const Ant MALE_ANT = {MALE_ANT_BODY, KNOWLEDGE, Ant::Type_e::Male};
+    const Ant MALE_ANT = {MALE_ANT_BODY, KNOWLEDGE, Ant::Action_e::Male};
 
     const Body QUEEN_ANT_BODY = {sf::Vector2f{-1.0f, -1.0f}, 4.0f, 4.0f,
                                   sf::Color{150, 0, 50, 255}};
-    const Ant QUEEN_ANT = {QUEEN_ANT_BODY, KNOWLEDGE, Ant::Type_e::Queen};
+    const Ant QUEEN_ANT = {QUEEN_ANT_BODY, KNOWLEDGE, Ant::Action_e::Queen};
 
 
 };
