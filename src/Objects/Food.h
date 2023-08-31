18f2e31cab81697e4dbbc038ee0824dc06b9dde4
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2023-09-01 00:05:23 +0300

    added the information necessary to know that a food is locked

--- src/Objects/Food.h
+++ src/Objects/Food.h
@@ -5,6 +5,10 @@ class Food
 {
 private:
     GenericObject<Food> m_genericObject;
+
+    //A food particle is locked if an ant is going to grab it, meaning that other ants cannot
+    //take this food particle; Furthermore, such food cannot decay anymore
+    bool m_isLocked = false;
 public:
     Food() = default;
     explicit Food(GenericObject<Food> genericObject);
@@ -18,5 +22,9 @@ public:
 
     GenericObject<Food> &genericObject();
     const GenericObject<Food> &genericObject() const;
+
+    bool &isLocked();
+    bool &isLocked() const;
+
 };
 
