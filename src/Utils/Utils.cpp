5cf7f39164a692a73b8c0ae58b99f01c07e749a0
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2023-09-05 15:12:45 +0300

    Optimised getShape by only modifing a base object, instead of re-creating it every time

--- src/Utils/Utils.cpp
+++ src/Utils/Utils.cpp
@@ -2,15 +2,15 @@
 
 sf::CircleShape getShape(const Body &body)
 {
+    static sf::CircleShape shape;
     float size = body.getSize();
-    sf::CircleShape shape(size);
+    shape.setRadius(size);
 
     const sf::Color &color = body.getColor();
     shape.setFillColor(color);
 
     sf::Vector2f position(body.getPosition());
     shape.setPosition(position);
-
     return shape;
 }
 
